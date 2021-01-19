be91786e4dc11ecc0009a9336ac60564dbe66ecf
Thibault MARCHAL <44288098+NoliaXeh@users.noreply.github.com>
2021-01-19 16:08:56 +0100

    Update AFMotor_ConstantSpeed.ino

--- AFMotor_ConstantSpeed.ino
+++ AFMotor_ConstantSpeed.ino
@@ -1,47 +1,102 @@
-// ConstantSpeed.pde
-// -*- mode: C++ -*-
-//
-// Shows how to run AccelStepper in the simplest,
-// fixed speed mode with no accelerations
-// Requires the AFMotor library (https://github.com/adafruit/Adafruit-Motor-Shield-library)
-// And AccelStepper with AFMotor support (https://github.com/adafruit/AccelStepper)
-// Public domain!
-
 #include <AFMotor.h>
-#include <Scheduler.h>
 
+#define JOYSTICK_ERROR 5
+
+const int UP    = A0;
+const int LEFT  = A2;
+const int DOWN  = A3;
+const int RIGHT = A1;
+const int JOY_Y = A4;
+const int JOY_X = A5;
+
+bool up = false;
+bool down = false;
+bool left = false;
+bool right = false;
+int direction = RELEASE;
+int speed = 0;
+int lval = 0;
+int rval = 0;
 
 AF_DCMotor motor1(1, MOTOR12_64KHZ);
 AF_DCMotor motor2(2, MOTOR12_64KHZ);
 AF_DCMotor motor3(3, MOTOR34_64KHZ);
 
 
-template <AF_DCMotor Motor, int speed>
-void setup_motor()
-{
-   Motor.setSpeed(speed);
-}
-
-template <AF_DCMotor Motor, int for_ms>
-void loop_motor()
-{
-    Motor.run(FORWARD);
-    delay(for_ms);
-    Motor.run(RELEASE);
-    delay(1000);
-}
+auto motor_left = motor1;
+auto motor_right = motor2;
+auto motor_up = motor3;
 
 void setup()
 {
-   //Serial.begin(9600);           // set up Serial library at 9600 bps
-   //Serial.println("Motor test!");
+  Serial.begin(115200);
+  motor_left.setSpeed(0);
+  motor_right.setSpeed(0);
+  motor_up.setSpeed(255);
+}
 
-   Scheduler.start(setup_motor<motor1, 5000>, loop_motor<motor1, 150>, 64);
-   Scheduler.start(setup_motor<motor2, 5000>, loop_motor<motor2, 150>, 64);
-   Scheduler.start(setup_motor<motor3, 5000>, loop_motor<motor3, 150>, 64);
+
+void readInputs()
+{
+    up = digitalRead(UP);
+    down = digitalRead(DOWN);
+    left = digitalRead(LEFT);
+    right = digitalRead(RIGHT);
+
+    double x = analogRead(JOY_X) - 512;
+    double y = analogRead(JOY_Y) - 512;
+
+    if (y > JOYSTICK_ERROR)
+    {
+        direction = FORWARD;
+        speed = max(150, y/2);
+    }
+    else if (y > -JOYSTICK_ERROR)
+    {
+        direction = RELEASE;
+        speed = 0;
+    }
+    else // y < -JOYSTICK_ERROR
+    {
+        direction = BACKWARD;
+        speed = max(150, (-y)/2);
+    }
+    rval = 155 + 50 + ((x/512.0)*50.0);
+    lval = 155 + 50 + ((-x/512.0)*50.0);
+}
+
+void debugInputs()
+{
+    Serial.print("up: ");Serial.print(up);
+    Serial.print(", down: ");Serial.print(down);
+    Serial.print(", left: ");Serial.print(left);
+    Serial.print(", right: ");Serial.print(right);
+    Serial.print(", rval: ");Serial.print(rval);
+    Serial.print(", lval: ");Serial.print(lval);
+    Serial.print(", dir: ");Serial.print(direction);
+    Serial.print(", speed: ");Serial.print(speed);
+    Serial.print("\n"); 
+}
+
+void updateMotors()
+{
+    motor_right.setSpeed(rval);
+    motor_left.setSpeed(lval);
+    motor_right.run(direction);
+    motor_left.run(direction);
+
+    if (up)
+        motor_up.run(FORWARD);
+    else if (down)
+        motor_up.run(BACKWARD);
+    else
+        motor_up.run(RELEASE);
 }
 
 void loop()
 {
-    yield();
+    readInputs();
+    debugInputs();
+    updateMotors();
+    delay(10);
 }
